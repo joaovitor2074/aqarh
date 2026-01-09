5111ef2a8fcfe9ac35dd9394904b7fb5747b98f1
Joao vitor <jvzinn2074@gmail.com>
2026-01-09 10:15:59 -0300

    iniciando notificscos

--- backend/src/seed.js
+++ backend/src/seed.js
@@ -2,11 +2,11 @@ import bcrypt from "bcrypt";
 import { db } from "./config/db.js";
 
 async function criarAdmin() {
-  const senhaCriptografada = await bcrypt.hash("123456", 10);
+  const senhaCriptografada = await bcrypt.hash("777", 10);
 
   await db.query(
     "INSERT INTO usuarios (nome, email, senha, cargo) VALUES (?, ?, ?, ?)",
-    ["Administrador", "admin@ifma.com", senhaCriptografada, "admin"]
+    ["DESENVOLVEOR", "jv@ifma.com", senhaCriptografada, "admin"]
   );
 
   console.log("Administrador criado!");
